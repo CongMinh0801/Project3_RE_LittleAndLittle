2f1e371dae3837285d69be6169828ab69f573d08
Congminh0801 <minh.207ct68653@vanlanguni.vn>
2023-05-05 13:45:56 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./assets/css/base.css"
+import { Navbar } from './layout/navbar/navbar';
+import { TagInfo } from './layout/tag_info/tag_info';
+import { ContactForm } from './layout/contact_form/contact_form';
+import { SendContactMessage } from './layout/send_contact_message/send_contact_message';
+import { PayFail } from './layout/pay_fail/pay_fail';
+import { PayTicketItem } from './layout/pay_ticket_item/pay_ticket_item';
+import { PayTicket } from './layout/pay_ticket/pay_ticket';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PayTicket/>
     </div>
   );
 }
